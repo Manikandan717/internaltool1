41835d29210ebbbade1e4e54c9ee58b4826f4026
Manikandan M <114073486+Manikandan717@users.noreply.github.com>
2023-12-27 12:45:43 +0530

    new

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -1,73 +1,66 @@
+
 import setAuthToken from 'utils/setAuthToken';
 import jwt_decode from 'jwt-decode';
-import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from './types';
+import {GET_ERRORS,SET_CURRENT_USER,USER_LOADING} from  './types';
 import axios from 'axios';
 
-export const registerUser = (userData) => (dispatch) => {
-  axios
-    .post('user/register', userData)
-    .then((res) => (window.location = '/authentication/sign-in'))
-    .catch((err) =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response ? err.response.data : 'An error occurred during registration',
-      })
-    );
+
+export const registerUser = (userData) => dispatch =>{
+    axios.post('user/register',userData)
+    .then(res=> window.location = '/authentication/sign-in')
+    .catch(err=>dispatch(
+        {
+            type: GET_ERRORS,
+            payload: err.response.data
+        }
+    ));
 };
 
-export const loginUser = (userData) => (dispatch) => {
-  axios
+export const loginUser = userData => dispatch => {
+    axios
     .post('user/login', userData)
-    .then((res) => {
-      // Save to localStorage
-      const { token } = res.data;
-
-      if (!token) {
-        // Handle the case where the token is not present
+    .then(res=>{
+         // Save to localStorage
+  
+        // Set token to localStorage
+        const { token }  = res.data;
+        localStorage.setItem("jwtToken", token);
+        // Set token to Auth header
+        setAuthToken(token);
+        // Decode token to get user data
+        const decoded = jwt_decode(token);
+        // Set current user
+        dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
         dispatch({
           type: GET_ERRORS,
-          payload: 'No token received during login',
-        });
-        return;
-      }
+          payload: err.response.data
+        })
+      );
+}
 
-      localStorage.setItem('jwtToken', token);
-      // Set token to Auth header
-      setAuthToken(token);
-      // Decode token to get user data
-      const decoded = jwt_decode(token);
-      // Set current user
-      dispatch(setCurrentUser(decoded));
-    })
-    .catch((err) =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response ? err.response.data : 'An error occurred during login',
-      })
-    );
-};
-
-// Action to set the current user
-export const setCurrentUser = (decoded) => {
-  return {
-    type: SET_CURRENT_USER,
-    payload: decoded,
+ // Set logged in user
+ export const setCurrentUser = decoded => {
+    return {
+      type: SET_CURRENT_USER,
+      payload: decoded
+    };
   };
-};
 
-// User loading
-export const setUserLoading = () => {
-  return {
-    type: USER_LOADING,
+  // User loading
+  export const setUserLoading = () => {
+    return {
+      type: USER_LOADING
+    };
   };
-};
 
-// Log user out
-export const logoutUser = () => (dispatch) => {
-  // Remove token from local storage
-  localStorage.removeItem('jwtToken');
-  // Remove auth header for future requests
-  setAuthToken(false);
-  // Set current user to empty object {} which will set isAuthenticated to false
-  dispatch(setCurrentUser({}));
-};
+   // Log user out
+   export const logoutUser = () => dispatch => {
+    // Remove token from local storage
+    localStorage.removeItem("jwtToken");
+    // Remove auth header for future requests
+    setAuthToken(false);
+    // Set current user to empty object {} which will set isAuthenticated to false
+    dispatch(setCurrentUser({}));
+  };
